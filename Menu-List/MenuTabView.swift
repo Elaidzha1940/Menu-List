1443b6dde1a20a8c5b431b09f2dfbe7d0f272cb8
Elaidzha1940 <eli080293@gmail.com>
2024-02-07 15:04:37 +0300

    Menu-List

---
+++ Menu-List/MenuTabView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Menu-List
+//  File: MenuTabView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.02.2024
+//
+//  */
+
+import SwiftUI
+
+struct MenuTabView: View {
+    var body: some View {
+        
+        TabView {
+            Text("Menu")
+        }
+    }
+}
+
+#Preview {
+    MenuTabView()
+}
---
+++ Menu-List/Menu_ListApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Menu-List
+//  File: Menu_ListApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.02.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct Menu_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuTabView()
+        }
+    }
+}
