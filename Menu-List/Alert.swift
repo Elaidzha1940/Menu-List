a206ed1249ebaa7c59f94234f08a691f99ba6a4a
Elaidzha1940 <eli080293@gmail.com>
2024-02-11 02:41:54 +0300

    Added sn Alert

--- Menu-List/Alert.swift
+++ Menu-List/Alert.swift
@@ -9,3 +9,27 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidURL       = AlertItem(title: Text("Server Error"),
+                                            message: Text("The data received from the server was invalid. Please contact support."),
+                                            dismissButton: .default(Text("Ok")))
+    
+    static let invalidResponse  = AlertItem(title: Text("Server Error"),
+                                            message: Text("Invalid response from the server. Please try again later or contact support."),
+                                            dismissButton: .default(Text("Ok")))
+    
+    static let invalidData      = AlertItem(title: Text("Server Error"),
+                                            message: Text("There was an issue connecting to the server. If "),
+                                            dismissButton: .default(Text("Ok")))
+    
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                            message: Text("Unable to complete your request at this time. Please check your internet connection."),
+                                            dismissButton: .default(Text("Ok")))
+}
--- Menu-List/ViewModels/MenuListViewModel.swift
+++ Menu-List/ViewModels/MenuListViewModel.swift
@@ -11,15 +11,25 @@ import SwiftUI
 
 final class MenuListViewModel: ObservableObject {
     @Published var menu: [MenuListModel] = []
+    @Published var alertItem: AlertItem?
     
     func getMenuList() {
         NetworkManager.shared.getMenu { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let menu):
                     self.menu = menu
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
