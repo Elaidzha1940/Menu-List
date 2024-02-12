a94c5551e8b01da94fcd078fb940449504d65ae1
Elaidzha1940 <eli080293@gmail.com>
2024-02-12 19:30:35 +0300

    Added MenuListDetailView

---
+++ Menu-List/Views/Detail/MenuListDetailView.swift
@@ -0,0 +1,20 @@
+//  /*
+//
+//  Project: Menu-List
+//  File: MenuListDetailView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.02.2024
+//
+//  */
+
+import SwiftUI
+
+struct MenuListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuListDetailView()
+}
