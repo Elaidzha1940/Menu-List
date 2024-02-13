96b91bdbafa132ba3aca1b9ac53dcea198fe0936
Elaidzha1940 <eli080293@gmail.com>
2024-02-13 16:03:12 +0300

    Update AccountView / Added a Toggles

--- Menu-List/Views/Main/AccountView.swift
+++ Menu-List/Views/Main/AccountView.swift
@@ -14,6 +14,8 @@ struct AccountView: View {
     @State private var lastName = ""
     @State private var email = ""
     @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
     
     var body: some View {
         
@@ -29,13 +31,19 @@ struct AccountView: View {
                     
                     DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
                         .accentColor(.brandPrimaryColor)
-                    
+
                     Button(action: {
                         //action
                     }, label: {
                         Text("Save changes")
                     })
                 }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimaryColor))
             }
             .navigationTitle("Account")
         }
