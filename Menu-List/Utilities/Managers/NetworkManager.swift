d3e94f56b518b206aec264167c150152fc8b2275
Elaidzha1940 <eli080293@gmail.com>
2024-02-19 00:13:47 +0300

    Update NetworkManager

--- Menu-List/Utilities/Managers/NetworkManager.swift
+++ Menu-List/Utilities/Managers/NetworkManager.swift
@@ -18,39 +18,58 @@ final class NetworkManager {
     
     private init() {}
     
-    func getMenu(completion: @escaping (Result<[MenuListModel], MError>) -> ()) {
+    //MARK: Before Asyn/await
+    //    func getMenu(completion: @escaping (Result<[MenuListModel], MError>) -> ()) {
+    //        guard let url = URL(string: menuURL) else {
+    //            completion(.failure(.invalidURL))
+    //            return
+    //        }
+    //
+    //        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+    //            if let _ = error {
+    //                completion(.failure(.unableToComplete))
+    //                return
+    //            }
+    //
+    //            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+    //                completion(.failure(.invalidResponse))
+    //                return
+    //            }
+    //
+    //            guard let data = data else {
+    //                completion(.failure(.invalidData))
+    //                return
+    //            }
+    //
+    //            do {
+    //                let decoder = JSONDecoder()
+    //                let decodedResponse = try decoder.decode(MenuListResponse.self, from: data)
+    //                completion(.success(decodedResponse.request))
+    //            } catch {
+    //                completion(.failure(.invalidData))
+    //            }
+    //        }
+    //
+    //        task.resume()
+    //    }
+    
+    
+    func getMenu() async throws -> [MenuListModel] {
         guard let url = URL(string: menuURL) else {
-            completion(.failure(.invalidURL))
-            return
+            throw MError.invalidURL
         }
         
-        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            if let _ = error {
-                completion(.failure(.unableToComplete))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(.failure(.invalidResponse))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let decodedResponse = try decoder.decode(MenuListResponse.self, from: data)
-                completion(.success(decodedResponse.request))
-            } catch {
-                completion(.failure(.invalidData))
-            }
-        }
+        let (data, response) = try await URLSession.shared.data(from: url)
         
-        task.resume()
+        do {
+            let decoder = JSONDecoder()
+            let decodedResponse = try decoder.decode(MenuListResponse.self, from: data)
+            return decodedResponse.request
+        } catch {
+            throw MError.invalidData
+        }
     }
+    
     
     func downloadImage(fromURLString urlString: String, completion: @escaping (UIImage?) -> ()) {
         let cacheKey = NSString(string: urlString)
