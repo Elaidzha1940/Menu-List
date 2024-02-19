7e795cfaaec0b19ec3d5d4d56b4158ae3b0e1f99
Elaidzha1940 <eli080293@gmail.com>
2024-02-19 22:43:02 +0300

    Update a new isValidEmail / Validation of email

--- Menu-List/Utilities/Extensions/String+Ext.swift
+++ Menu-List/Utilities/Extensions/String+Ext.swift
@@ -8,12 +8,38 @@
 //  */
 
 import SwiftUI
+import RegexBuilder
 
 extension String {
     
     var isValidEmail: Bool {
-        let emailFormat         = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
-        return emailPredicate.evaluate(with: self)
+        let emailRegex = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        
+        return self.wholeMatch(of: emailRegex) != nil
     }
 }
