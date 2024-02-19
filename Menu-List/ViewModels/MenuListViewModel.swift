7f94c3abd311ef7a6c49fe3d66525a6716ef953d
Elaidzha1940 <eli080293@gmail.com>
2024-02-19 17:41:46 +0300

    Update MenuListViewModel

--- Menu-List/ViewModels/MenuListViewModel.swift
+++ Menu-List/ViewModels/MenuListViewModel.swift
@@ -9,34 +9,61 @@
 
 import SwiftUI
 
-final class MenuListViewModel: ObservableObject {
+@MainActor final class MenuListViewModel: ObservableObject {
     @Published var menu: [MenuListModel] = []
     @Published var alertItem: AlertItem?
     @Published var isLoading = false
     @Published var isShowingDetail = false
     @Published var selectedMenuListModel: MenuListModel?
     
+    //    func getMenuList() {
+    //        isLoading = true
+    //        NetworkManager.shared.getMenu { [self] result in
+    //            DispatchQueue.main.async { [self] in
+    //                isLoading = false
+    //                switch result {
+    //                case .success(let menu):
+    //                    self.menu = menu
+    //
+    //                case .failure(let error):
+    //                    switch error {
+    //                    case .invalidResponse:
+    //                        alertItem = AlertContext.invalidResponse
+    //                    case .invalidURL:
+    //                        alertItem = AlertContext.invalidURL
+    //                    case .invalidData:
+    //                        alertItem = AlertContext.invalidData
+    //                    case .unableToComplete:
+    //                        alertItem = AlertContext.unableToComplete
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    
     func getMenuList() {
         isLoading = true
-        NetworkManager.shared.getMenu { [self] result in
-            DispatchQueue.main.async { [self] in
+        
+        Task {
+            do {
+                menu = try await NetworkManager.shared.getMenu()
                 isLoading = false
-                switch result {
-                case .success(let menu):
-                    self.menu = menu
-                    
-                case .failure(let error):
-                    switch error {
-                    case .invalidResponse:
-                        alertItem = AlertContext.invalidResponse
+            } catch {
+                if let mError = error as? MError {
+                    switch mError {
                     case .invalidURL:
                         alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
                     case .invalidData:
                         alertItem = AlertContext.invalidData
                     case .unableToComplete:
                         alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidResponse
                 }
+                isLoading = false
             }
         }
     }
